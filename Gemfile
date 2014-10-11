4b06910375e1f477f6605a85fb93df7a51300247
Zaven Gabriel <zgabriel@alumni.cmu.edu>
2014-10-11 17:21:12 +0000

    Lesson 12: playing with routes, new quote form, home links working.

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 
+gem 'simple_form'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Splurty::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :quotes
+
   # Example resource route with options:
   #   resources :products do
   #     member do
